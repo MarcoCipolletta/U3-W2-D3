847b9c8e7262228fd7d5dec262356bf4f788d0f2
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-16 14:31:52 +0200

    Set up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { PreferitiComponent } from './pages/preferiti/preferiti.component';
+import { NavbarComponent } from './mainComponents/navbar/navbar.component';
+import { CartComponent } from './mainComponents/cart/cart.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PreferitiComponent,
+    NavbarComponent,
+    CartComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
